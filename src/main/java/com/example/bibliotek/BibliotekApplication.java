ef5c8b52f2343028b053860e72ec96dc601eefcf
AndreyFan <fesha1976@gmail.com>
2024-11-26 15:37:05 +0100

    25nov2024

--- src/main/java/com/example/bibliotek/BibliotekApplication.java
+++ src/main/java/com/example/bibliotek/BibliotekApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BibliotekApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BibliotekApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BibliotekApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/bibliotek/entity/enums/Genre.java
+++ src/main/java/com/example/bibliotek/entity/enums/Genre.java
@@ -1,4 +1,10 @@
 package com.example.bibliotek.entity.enums;
 
 public enum Genre {
+
+    FICTION,
+    NON_FICTION,
+    POETRY,
+    DRAMA,
+    FANTASY
 }
